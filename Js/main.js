37cae5f91d48ce1d5c45242f5deeb4d3097956c6
JianiHuang <hjiani@umich.edu>
2026-03-29 22:34:33 -0400

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -3,15 +3,3 @@ const currentPage = window.location.pathname.split("/").pop() || "index.html";
 if (currentPage === "index.html") {
   alert("Welcome to UMSI Career Hub!");
 }
-
-if (currentPage === "resume.html") {
-  alert("Welcome to UMSI Resume Support!");
-}
-
-if (currentPage === "Networking.html") {
-  alert("Welcome to UMSI Networking and Connections Hub!");
-}
-
-if (currentPage === "career-exploration.html") {
-  alert("Welcome to UMSI Career Exploration Hub!");
-}
